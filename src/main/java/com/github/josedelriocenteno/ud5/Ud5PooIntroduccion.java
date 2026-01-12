77a9baf534d6143f5d2da0f5fe0e86945a62bfe1
josedelriocenteno <josemiguel.delrio.centeno@gregoriofer.com>
2026-01-12 17:02:55 +0100

    Inicial commit Ud5 POO Introducción

---
+++ src/main/java/com/github/josedelriocenteno/ud5/Ud5PooIntroduccion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.josedelriocenteno.ud5;
+
+/**
+ *
+ * @author delcenjo
+ */
+public class Ud5PooIntroduccion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
